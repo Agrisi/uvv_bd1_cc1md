d4883b961f16658ecadb9a4d5e6bfe8b5b7bc0cc
Cauã Agrisi <127743223+melby-law@users.noreply.github.com>
2023-05-28 15:44:17 -0300

    Update cc1md_202309352_postgresql.sql

--- pset1/cc1md_202309352_postgresql.sql
+++ pset1/cc1md_202309352_postgresql.sql
@@ -1,13 +1,18 @@
+--deletando a database/esquema/usuario com o mesmo nome se já existirem--
 DROP DATABASE IF EXISTS uvv;
 
 DROP SCHEMA IF EXISTS lojas CASCADE;
 
 DROP USER IF EXISTS caua;
 
+--criação de um usuário com meu nome--
+
 CREATE USER caua 
 WITH ENCRYPTED PASSWORD 'xlr8ult'
 CREATEDB
 CREATEROLE login;
+
+--criação do banco de dados uvv--
 
 CREATE DATABASE uvv 
 OWNER = caua
@@ -17,12 +22,17 @@ LC_COLLATE = 'pt_BR.UTF-8'
 LC_CTYPE = 'pt_BR.UTF-8'
 ALLOW_CONNECTIONS = true;
 
+--conectando ao banco de dados "uvv"--
+
 \c "host=localhost dbname=uvv user=caua password=xlr8ult"
+
+--criando o esquema lojas e alterando a mesma para a posse do meu usuário--
 
 CREATE SCHEMA lojas;
 
 ALTER SCHEMA lojas OWNER TO caua;
 
+--Criação da tabela produtos e de comentários sobre a tabela e suas colunas--
 
 CREATE TABLE    lojas.produtos (
                 produto_id       NUMERIC(38) NOT NULL,
@@ -47,6 +57,7 @@ COMMENT ON COLUMN lojas.produtos.imagem_arquivo            IS 'Coluna que conté
 COMMENT ON COLUMN lojas.produtos.imagem_charset            IS 'Coluna que contém o charset de cada imagem.';
 COMMENT ON COLUMN lojas.produtos.imagem_ultima_atualizacao IS 'Coluna que contém a data da ultima atualização de cada imagem.';
 
+--Criação da tabela lojas e de comentários sobre a tabela e suas colunas--
 
 CREATE TABLE lojas.lojas (
                 loja_id         NUMERIC(38) NOT NULL,
@@ -76,6 +87,7 @@ COMMENT ON COLUMN lojas.lojas.logo_arquivo             IS 'Coluna que contém o 
 COMMENT ON COLUMN lojas.lojas.logo_charset             IS 'Coluna que mostra o charset das logos.';
 COMMENT ON COLUMN lojas.lojas.logo_ultima_atualizacao  IS 'Coluna que contém a data da ultima atualização de cada logo.';
 
+--Criação da tabela estoques e de comentários sobre a tabela e suas colunas--
 
 CREATE TABLE lojas.estoques (
                 estoque_id NUMERIC(38) NOT NULL,
@@ -90,6 +102,7 @@ COMMENT ON COLUMN lojas.estoques.loja_id    IS 'Coluna com o número de identifi
 COMMENT ON COLUMN lojas.estoques.produto_id IS 'Coluna que contém a identificação dos produtos.';
 COMMENT ON COLUMN lojas.estoques.quantidade IS 'Coluna que contém a quantidade de produtos que cada estoque tem.';
 
+--Criação da tabela clientes e de comentários sobre a tabela e suas colunas--
 
 CREATE TABLE lojas.clientes (
                 cliente_id NUMERIC(38) NOT NULL,
@@ -108,6 +121,7 @@ COMMENT ON COLUMN lojas.clientes.telefone1  IS 'Coluna contendo um número de te
 COMMENT ON COLUMN lojas.clientes.telefone2  IS 'Coluna contendo um número de telefone que o cliente tem acesso.';
 COMMENT ON COLUMN lojas.clientes.telefone3  IS 'Coluna contendo um número de telefone que o cliente tem acesso.';
 
+--Criação da tabela envios e de comentários sobre a tabela e suas colunas--
 
 CREATE TABLE lojas.envios (
                 envio_id         NUMERIC(38) NOT NULL,
@@ -125,6 +139,7 @@ COMMENT ON COLUMN lojas.envios.cliente_id       IS 'Coluna com o número de iden
 COMMENT ON COLUMN lojas.envios.endereco_entrega IS 'Coluna que contém o endereço em que o produto foi enviado.';
 COMMENT ON COLUMN lojas.envios.status           IS 'Coluna que contém os status dos envios da loja.';
 
+--Criação da tabela pedidos e de comentários sobre a tabela e suas colunas--
 
 CREATE TABLE lojas.pedidos (
                 pedido_id  NUMERIC(38) NOT NULL,
@@ -142,6 +157,7 @@ COMMENT ON COLUMN lojas.pedidos.cliente_id IS 'Coluna com o número de identific
 COMMENT ON COLUMN lojas.pedidos.status     IS 'Coluna que contém o status dos pedidos.';
 COMMENT ON COLUMN lojas.pedidos.loja_id    IS 'Coluna com o número de identificação das lojas.';
 
+--Criação da tabela pedidos_itens e de comentários sobre a tabela e suas colunas--
 
 CREATE TABLE lojas.pedidos_itens (
                 produto_id      NUMERIC(38) NOT NULL,
@@ -160,6 +176,7 @@ COMMENT ON COLUMN lojas.pedidos_itens.numero_da_linha IS 'Coluna que contém o n
 COMMENT ON COLUMN lojas.pedidos_itens.preco_unitario  IS 'Coluna que contém o preço unitário dos produtos pedidos.';
 COMMENT ON COLUMN lojas.pedidos_itens.quantidade      IS 'Coluna que contém a quantidade dos itens pedidos.';
 
+ --Adicionando a FK produto_id (da tabela produtos) na tabela estoques--
 
 ALTER TABLE lojas.estoques ADD CONSTRAINT produtos_estoques_fk
 FOREIGN KEY (produto_id)
@@ -168,12 +185,18 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
+
+ --Adicionando a FK produto_id (da tabela produtos) na tabela pedido_itens--
+
 ALTER TABLE lojas.pedidos_itens ADD CONSTRAINT produtos_pedidos_itens_fk
 FOREIGN KEY (produto_id)
 REFERENCES lojas.produtos (produto_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
+
+
+ --Adicionando a FK loja_id (da tabela lojas) na tabela envios--
 
 ALTER TABLE lojas.envios ADD CONSTRAINT lojas_envios_fk
 FOREIGN KEY (loja_id)
@@ -182,12 +205,18 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
+
+ --Adicionando a FK loja_id (da tabela lojas) na tabela estoques--
+
 ALTER TABLE lojas.estoques ADD CONSTRAINT lojas_estoques_fk
 FOREIGN KEY (loja_id)
 REFERENCES lojas.lojas (loja_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
+
+
+ --Adicionando a FK loja_id (da tabela lojas) na tabela pedidos--
 
 ALTER TABLE lojas.pedidos ADD CONSTRAINT lojas_pedidos_fk
 FOREIGN KEY (loja_id)
@@ -196,12 +225,18 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
+
+ --Adicionando a FK cliente_id (da tabela clientes) na tabela envios--
+
 ALTER TABLE lojas.envios ADD CONSTRAINT clientes_envios_fk
 FOREIGN KEY (cliente_id)
 REFERENCES lojas.clientes (cliente_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
+
+
+ --Adicionando a FK cliente_id (da tabela clientes) na tabela pedidos--
 
 ALTER TABLE lojas.pedidos ADD CONSTRAINT clientes_pedidos_fk
 FOREIGN KEY (cliente_id)
@@ -210,6 +245,9 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
+
+ --Adicionando a FK pedido_id (da tabela pedidos) na tabela pedidos_itens--
+
 ALTER TABLE lojas.pedidos_itens ADD CONSTRAINT pedidos_pedidos_itens_fk
 FOREIGN KEY (pedido_id)
 REFERENCES lojas.pedidos (pedido_id)
@@ -217,9 +255,18 @@ ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
 
+
+ --Adicionando a FK envio_id (da tabela envios) na tabela pedidos_itens--
+
 ALTER TABLE lojas.pedidos_itens ADD CONSTRAINT envios_pedidos_itens_fk
 FOREIGN KEY (envio_id)
 REFERENCES lojas.envios (envio_id)
 ON DELETE NO ACTION
 ON UPDATE NO ACTION
 NOT DEFERRABLE;
+
+--Para ser sincero, achei o PSET mais desafiador do que me haviam dito.
+O fato de eu ter um conhecimento limitado sobre o assunto tornou a tarefa mais difícil. No entanto, ao realizá-lo, pude aprender muito e crescer tanto como indivíduo, adquirindo um senso de responsabilidade e um desejo genuíno de aprender, quanto como cientista da computação.
+Tenho a convicção de que um dia serei capaz de dominar completamente essa matéria e aplicar meu conhecimento de forma eficaz em projetos futuros.--
+--Créditos: Cauã Agrisi Merisio--
+--Matricula: 202309352--
